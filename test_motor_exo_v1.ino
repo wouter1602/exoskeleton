0091f4adcb238e9d883a894c444739110e95ab4f
Wouter <wouter@velzen.cc>
2021-04-14 09:36:48 +0200

    Rename functions and variable to follow Camel-casing
    Added documentation for every function

--- test_motor_exo_v1.ino
+++ test_motor_exo_v1.ino
@@ -21,13 +21,13 @@
  * Motor struct with all the values from an single motor combined.
  */
 struct Motor {
-  uint8_t pwm_pin = 0;
-  uint8_t pwm_value = 160;
-  uint8_t enable_rotate_left_pin = 0;
-  uint8_t enable_rotate_right_pin = 0;
-  bool rot_direction = true;            // True is rotating clockwise, false is counter clockwise.
-  uint8_t potentiometer = 0;
-  uint16_t ADC_value = 0;
+    uint8_t pwmPin = 0;
+    uint8_t pwmValue = 160;
+    uint8_t enableRotateLeftPin = 0;
+    uint8_t enableRotateRightPin = 0;
+    bool rot_direction = true;            // True is rotating clockwise, false is counter clockwise.
+    uint8_t potentiometer = 0;
+    uint16_t ADCValue = 0;
 };
 
 /**
@@ -37,102 +37,116 @@ Motor upper_motor;
 Motor lower_motor;
 
 /**
+ * @brief Setup Motor I/O
  * Function sets the inputs and outputs used by the motor driver
  * @param motor Motor struct with the corresponding pins
- * @return void
  */
-void setup_motor(const Motor &motor) {
-    pinMode(motor.pwm_pin, OUTPUT);
-    pinMode(motor.enable_rotate_left_pin, OUTPUT);
-    pinMode(motor.enable_rotate_right_pin, OUTPUT);
+void setupMotor(const Motor &motor) {
+    pinMode(motor.pwmPin, OUTPUT);
+    pinMode(motor.enableRotateLeftPin, OUTPUT);
+    pinMode(motor.enableRotateRightPin, OUTPUT);
     pinMode(motor.potentiometer, INPUT);
 
     // Initial direction
-    digitalWrite(motor.enable_rotate_left_pin, LOW);
-    digitalWrite(motor.enable_rotate_right_pin, HIGH);
+    digitalWrite(motor.enableRotateLeftPin, LOW);
+    digitalWrite(motor.enableRotateRightPin, HIGH);
 }
 
 /**
+ * @brief Set the Motor Speed and orientation
  * If motor speed is set to 0 this function stops the motor
  * Otherwise it set the orientation and the give speed in the Motor struct
  * @param motor Motor struct with the pins, rotation and speed
- * @return void
  */
-void set_motor_speed(const Motor &motor) {
-    if (motor.pwm_value == 0) {                             // Motor should not rotate
-        analogWrite(motor.pwm_pin, motor.pwm_value);        // Disable PWM signal
-        digitalWrite(motor.enable_rotate_left_pin, LOW);    // Disable left enable
-        digitalWrite(motor.enable_rotate_right_pin, LOW);   // Disable right enable
+void setMotorSpeed(const Motor &motor) {
+    if (motor.pwmValue == 0) {                          // Motor should not rotate
+        analogWrite(motor.pwmPin, motor.pwmValue);      // Disable PWM signal
+        digitalWrite(motor.enableRotateLeftPin, LOW);   // Disable left enable
+        digitalWrite(motor.enableRotateRightPin, LOW);  // Disable right enable
         return;
     }
-    if (motor.rot_direction) {                              // Rotate right
-        digitalWrite(motor.enable_rotate_left_pin, LOW);    // Disable left enable
-        digitalWrite(motor.enable_rotate_right_pin, HIGH);  // Enable right enable
-    } else {                                                // Rotate left
-        digitalWrite(motor.enable_rotate_left_pin, HIGH);   // Enable left enable
-        digitalWrite(motor.enable_rotate_right_pin, LOW);   // Disable right enable
+    if (motor.rot_direction) {                          // Rotate right
+        digitalWrite(motor.enableRotateLeftPin, LOW);   // Disable left enable
+        digitalWrite(motor.enableRotateRightPin, HIGH); // Enable right enable
+    } else {                                            // Rotate left
+        digitalWrite(motor.enableRotateLeftPin, HIGH);  // Enable left enable
+        digitalWrite(motor.enableRotateRightPin, LOW);  // Disable right enable
     }
 
-    analogWrite(motor.pwm_pin, motor.pwm_value);            // Set motor speed
+    analogWrite(motor.pwmPin, motor.pwmValue);          // Set motor speed
 }
 
 /**
+ * @brief Read angle of the motor
  * Reads ADC value of potentiometer defined in the Motor struct.
  * ADC value is saved in the ADC_value in the struct.
  * @param motor Motor struct with the potentiometer and ADC value
- * @return void 
  */
-void read_potentiometer(Motor &motor) {
-    motor.ADC_value = analogRead(motor.potentiometer);      // Read ADC value
+void readPotentiometer(Motor &motor) {
+    motor.ADCValue = analogRead(motor.potentiometer);      // Read ADC value
 }
 
+/**
+ * @brief Move motor to target angle
+ * The function mesures the current angle the motor has.
+ * It checks if the motor should rotate left or right and enables the motor with the given PWM speed.
+ * If the motor angle is the same as the targetAngle within the offset the motor will be disabled.
+ * @param motor Motor struct with the motor settings
+ * @param targetAngle Angle the motor should move to
+ * @param offset Mesurement offset
+ * @param speed PWM speed the motor should move
+ */
 void goToTargetAngle(Motor &motor, uint16_t targetAngle, uint8_t offset, uint8_t speed) {
-    read_potentiometer(motor);                              // Read Potentiometer
-    if ((motor.ADC_value - offset) > targetAngle) {         // If true rotate clockwise
+    readPotentiometer(motor);                              // Read Potentiometer
+    if ((motor.ADCValue - offset) > targetAngle) {         // If true rotate clockwise
         motor.rot_direction = true;
-        motor.pwm_value = speed;
-        set_motor_speed(motor);
-    } else if ((motor.ADC_value + offset) < targetAngle) {  // If true rotate counter clockswise
+        motor.pwmValue = speed;
+        setMotorSpeed(motor);
+    } else if ((motor.ADCValue + offset) < targetAngle) {  // If true rotate counter clockswise
         motor.rot_direction = false;
-        motor.pwm_value = speed;
-        set_motor_speed(motor);
+        motor.pwmValue = speed;
+        setMotorSpeed(motor);
     } else {                                                // If true should stop rotating
-        motor.pwm_value = 0;
-        set_motor_speed(motor);
+        motor.pwmValue = 0;
+        setMotorSpeed(motor);
     }
     
 }
 
 /**
- * Set up the I/O of the Arduino.
- * @return void
+ * @brief Setup the I/O of the Arduino
+ * Setup motor values pin numbers and Serial for debuging.
  */
 void setup() {
-  upper_motor.pwm_pin = MOTOR_1_PWM_PIN;                    // PWM pin
-  upper_motor.pwm_value = 160;                               // Default motor speed
-  upper_motor.enable_rotate_left_pin = MOTOR_1_LEFT_EN;     // Enable left rotation pin
-  upper_motor.enable_rotate_right_pin = MOTOR_1_RIGHT_EN;   // Enable right rotation pin
-  upper_motor.potentiometer = POTENT_1;                     // Potentiometer ADC pin
+  upper_motor.pwmPin = MOTOR_1_PWM_PIN;                 // PWM pin
+  upper_motor.pwmValue = 160;                           // Default motor speed
+  upper_motor.enableRotateLeftPin = MOTOR_1_LEFT_EN;    // Enable left rotation pin
+  upper_motor.enableRotateRightPin = MOTOR_1_RIGHT_EN;  // Enable right rotation pin
+  upper_motor.potentiometer = POTENT_1;                 // Potentiometer ADC pin
 
-  lower_motor.pwm_pin = MOTOR_2_PWM_PIN;                    // PWM pin
-  lower_motor.pwm_value = 160;                               // Default motor speed
-  lower_motor.enable_rotate_left_pin = MOTOR_2_LEFT_EN;     // Enable left rotation pin
-  lower_motor.enable_rotate_right_pin = MOTOR_2_RIGHT_EN;   // Enable right rotation pin
-  lower_motor.potentiometer = POTENT_2;                     // Potentiometer ADC pin
+  lower_motor.pwmPin = MOTOR_2_PWM_PIN;                 // PWM pin
+  lower_motor.pwmValue = 160;                           // Default motor speed
+  lower_motor.enableRotateLeftPin = MOTOR_2_LEFT_EN;    // Enable left rotation pin
+  lower_motor.enableRotateRightPin = MOTOR_2_RIGHT_EN;  // Enable right rotation pin
+  lower_motor.potentiometer = POTENT_2;                 // Potentiometer ADC pin
 
-  setup_motor(upper_motor);                                 // Set input output for upper motor
-  setup_motor(lower_motor);                                 // Set input output for lower motor
+  setupMotor(upper_motor);                              // Set input output for upper motor
+  setupMotor(lower_motor);                              // Set input output for lower motor
 
   Serial.begin(115200);
 }
 
+/**
+ * @brief Arduino loop function
+ * rotates the motors of the exoselton
+ */
 void loop() {
-    read_potentiometer(upper_motor);                        // Read upper motor ADC value
-    read_potentiometer(lower_motor);                        // Read lower motor ADC value
+    readPotentiometer(upper_motor);                     // Read upper motor ADC value
+    readPotentiometer(lower_motor);                     // Read lower motor ADC value
 
-    Serial.print(upper_motor.ADC_value);                    // Print ADC values for debugging
+    Serial.print(upper_motor.ADCValue);                 // Print ADC values for debugging
     Serial.print(", ");
-    Serial.println(lower_motor.ADC_value);
+    Serial.println(lower_motor.ADCValue);
 
     // set_motor_speed(upper_motor);
 
